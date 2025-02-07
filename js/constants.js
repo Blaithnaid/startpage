f66bcb544b2ba803a64c0cb55d25114efea6b0c7
Iarla Sparrow Burke <36054744+Blaithnaid@users.noreply.github.com>
2025-02-07 03:51:57 +0000

    separating things a bit better

---
+++ js/constants.js
@@ -0,0 +1,79 @@
+const images = [
+	"./assets/1.jpeg",
+	"./assets/2.jpeg",
+	"./assets/3.jpeg",
+	"./assets/4.jpeg",
+	"./assets/5.jpeg",
+	"./assets/6.jpeg",
+	"./assets/7.jpeg",
+	"./assets/8.jpeg",
+	"./assets/9.jpeg",
+	"./assets/10.jpeg",
+	"./assets/11.jpeg",
+];
+const links = [
+	{
+		title: "github",
+		url: "https://github.com",
+		color: "blue-500",
+	},
+	{
+		title: "moodle",
+		url: "https://vlegalwaymayo.atu.ie",
+		color: "teal-600",
+	},
+	{
+		title: "youtube",
+		url: "https://youtube.com",
+		color: "red-500",
+	},
+	{
+		title: "coolors",
+		url: "https://coolors.co/generate",
+		color: "blue-600",
+	},
+	{
+		title: "firebase",
+		url: "https://console.firebase.google.com",
+		color: "orange-400",
+	},
+	{
+		title: "last.fm",
+		url: "https://last.fm",
+		color: "red-500",
+	},
+	{
+		title: "stackoverflow",
+		url: "https://stackoverflow.com",
+		color: "orange-500",
+	},
+	{
+		title: "nyt games",
+		url: "https://www.nytimes.com/games",
+		color: "blue-500",
+	},
+];
+const searchoptions = [
+	{
+		title: "wikipedia",
+		url: "https://en.wikipedia.org/w/index.php?title=Special:Search&search=%s",
+	},
+	{
+		title: "letterboxd",
+		url: "https://letterboxd.com/search/%s",
+	},
+	{
+		title: "rateyourmusic",
+		url: "https://rateyourmusic.com/search?searchterm=%s",
+	},
+	{
+		title: "duckduckgo",
+		url: "https://duckduckgo.com/?q=%s",
+	},
+	{
+		title: "google",
+		url: "https://www.google.com/search?q=%s",
+	},
+];
+
+export { images, links, searchoptions };
